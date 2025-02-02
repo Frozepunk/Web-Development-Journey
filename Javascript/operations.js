09fd2535f5c337db61cfa6b3769fb838bb6e8bf7
Frozepunk <smg2750935@gmail.com>
2025-02-02 16:19:41 +0530

    Now let's do some coding and learning

---
+++ Javascript/operations.js
@@ -0,0 +1,15 @@
+let add=5+6
+let sub=8-9
+let mul=8*9
+let div=8/5
+let remendair = 8%9
+let expo=2**3
+let myscore=90
+myscore++
+let dop=89
+dop--
+console.log(add,sub,mul,div,remendair,expo,myscore,dop);
+// Comparision operation 
+// ==
+// !=
+// Find out the difference between == and === in javascript 
