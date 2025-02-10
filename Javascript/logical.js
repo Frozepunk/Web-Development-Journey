874e80610702faf41fcb1849d5488cbb40bdbf70
PRAJWAL.N <113663135+Frozepunk@users.noreply.github.com>
2025-02-10 10:28:42 +0000

    learning and updating the repositry

---
+++ Javascript/logical.js
@@ -0,0 +1,13 @@
+// These are the logical operators 
+// && -> and 
+// || -> or 
+// ! not or reverse
+let isLoggedin = true;
+let ispaid = true;
+console.log(isLoggedin);
+console.log(ispaid);
+let isEmailuser=true;
+let isGoogleuser=true;
+console.log(isEmailuser || isGoogleuser);
+
+
